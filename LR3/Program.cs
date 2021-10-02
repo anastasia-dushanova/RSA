8cab9b792237946f97ff2b9809e117bac8b7a90e
dushanova45 <dushanova45@gmail.com>
2021-10-02 22:09:57 +0300

    попытка зашифровать

--- LR3/Program.cs
+++ LR3/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Numerics;
 
 namespace LR3
 {
+
     class Program
     {
+        
+
+
         static void Main(string[] args)
         { //подготовка ключей. Шаг первый
             //var rnd_p = GetRandom();
@@ -24,6 +30,65 @@ namespace LR3
             Console.WriteLine("Определим число e = {0} ", e);
             Console.WriteLine("(e,n) = ({0},{1}) - открытый ключ", e, n);
             Console.WriteLine("(d,n) = ({0},{1}) - закрытый ключ", d, n);
+            string text = File.ReadAllText("input.txt");
+            text = text.ToUpper();
+            List<string> shifr = Shifr(text, e, n);
+            Console.WriteLine();
+            string result = Rashifr(shifr, d, n);
+            
+        }
+        public static string Rashifr(List<string> input, long d, long n)
+        {
+            char[] arr = new char[] { '#', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+                                                'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+                                                'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+                                                'Э', 'Ю', 'Я', ' ', '1', '2', '3', '4', '5', '6', '7',
+                                                '8', '9', '0' };
+            string result = "";
+
+            BigInteger bi;
+
+            foreach (string item in input)
+            {
+                bi = new BigInteger(Convert.ToDouble(item));
+                bi = BigInteger.Pow(bi, (int)d);
+
+                BigInteger n_ = new BigInteger((int)n);
+
+                bi = bi % n_;
+
+                int index = Convert.ToInt32(bi.ToString());
+
+                result += arr[index].ToString();
+            }
+            Console.WriteLine(result);
+            File.WriteAllText("output.txt", result);
+            return result;
+        }
+        public static List<string> Shifr(string text, long e, long n) //шифруем
+        {
+            char[] arr = new char[] { '#', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+                                                'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+                                                'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+                                                'Э', 'Ю', 'Я', ' ', '1', '2', '3', '4', '5', '6', '7',
+                                                '8', '9', '0' };
+            BigInteger bi;
+            List<string> result = new List<string>();
+            for(int i=0; i<text.Length; i++)
+            {
+                int index = Array.IndexOf(arr, text[i]);
+                bi = new BigInteger(index);
+                bi = BigInteger.Pow(bi, (int)e);
+
+                BigInteger n_ = new BigInteger((int)n);
+
+                bi = bi % n_;
+
+                result.Add(bi.ToString());
+            }
+            foreach(string s in result)
+                 Console.Write("{0} ", s);
+            return result;
         }
         public static long Calc_d(long f) //вычисление параметра d взамнопростое с f
         {
